acee35c3b862ca8b9cf5d022f5be0722c237b9ef
Addy Rodriguez <rdz.33.a@gmail.com>
2022-05-13 22:35:23 -0500

    Click shows option choosen on screen

---
+++ app.js
@@ -0,0 +1,10 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('my-choice')
+const resultDisplay = document.getElementById('winner')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+}))
