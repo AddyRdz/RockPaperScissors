83937573353e6364bf8e4c72127c28d3586c63a3
Addy Rodriguez <rdz.33.a@gmail.com>
2022-05-16 21:48:06 -0500

    Winner is now visible

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const userChoiceDisplay = document.getElementById('my-choice')
 const resultDisplay = document.getElementById('winner')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
+let winner
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getWinner()
 }))
 
 function generateComputerChoice(){
@@ -23,4 +26,29 @@ function generateComputerChoice(){
         computerChoice = "Paper"
     }
     computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getWinner(){
+    if (computerChoice === userChoice){
+        result = "It is a tie!"
+    }
+    if (computerChoice === 'Rock' && userChoice === 'Paper'){
+        result = "You Win!"
+    }
+    if (computerChoice === 'Rock' && userChoice === 'Scissors'){
+        result = "You Lose!"
+    }
+    if (computerChoice === 'Paper' && userChoice === 'Rock'){
+        result = "You Lose!"
+    }
+    if (computerChoice === 'Paper' && userChoice === 'Scissors'){
+        result = "You Win!"
+    }
+    if (computerChoice === 'Scissors' && userChoice === 'Paper'){
+        result = "You Lose!"
+    }
+    if (computerChoice === 'Scissors' && userChoice === 'Rock'){
+        result = "You Win!"
+    }
+    resultDisplay.innerHTML = result
 }
